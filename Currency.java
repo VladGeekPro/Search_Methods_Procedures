b77bd58572ae71d6db462e2d58eab063ea8eb18f
VladGeekPro <126159146+VladGeekPro@users.noreply.github.com>
2023-03-30 16:25:51 +0300

    Add files via upload

---
+++ Currency.java
@@ -0,0 +1,26 @@
+
+
+public class Currency {
+    
+    String cname1;
+    String cname2;
+    String data;
+    Double value;
+    Integer number;
+
+    public Currency(){};
+
+    public Currency(String c1,String c2, String d, double v, int n ) 
+    {
+    this.cname1 = c1;
+    this.cname2 = c2;
+    this.data = d;
+    this.value = v;
+    this.number = n;
+    }
+
+
+
+
+   
+}
